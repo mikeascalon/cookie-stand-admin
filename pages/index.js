845910cb5608d068f600e05e07a55a8f676f09a1
mikeascalon <mikeascalon@yahoo.com>
2024-02-28 22:19:01 -0800

    added hourly table

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head';
 import React, { useState } from 'react';
+import Header from '@/components/header.js';
+import Footer from '@/components/footer.js';
+import QuestionForm from '@/components/questionForm.js';
+import CookieStandTable from '@/components/cookieStandTable.js';
+import { hours } from '../data';
+
 
 function Main(props) {
   return <main>{props.children}</main>;
@@ -15,10 +21,13 @@ export default function Home() {
     event.preventDefault();
     const newStand = {
       location: event.target.location.value,
-      minCustomers: event.target.minCustomers.value,
-      maxCustomers: event.target.maxCustomers.value,
-      avgCookies: event.target.avgCookies.value,
+      minCustomers: parseInt(event.target.minCustomers.value),
+      maxCustomers: parseInt(event.target.maxCustomers.value),
+      avgCookies: parseFloat(event.target.avgCookies.value),
+      hourly_sales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36],
+
     };
+      
     setCookieStands([...cookieStands, newStand]);
     setLastLocation(newStand.location); 
     event.target.reset();
@@ -35,7 +44,7 @@ export default function Home() {
       <Header />
       <Main>
         <QuestionForm onSubmit={handleSubmit} lastLocation={lastLocation} />
-        <CookieStandTable cookieStands={cookieStands} />
+        <CookieStandTable cookieStands={cookieStands} hours={hours} />
         
       </Main>
       <Footer />
@@ -44,72 +53,6 @@ export default function Home() {
 }
 
 
-function Header() {
-  return (
-    <header className='p-4 text-4xl bg-green-900 text-gray-50'>
-      <h1>Cookie Stand Admin</h1>
-    </header>
-  );
-}
-
-function QuestionForm(props) {
-  return (
-    <form onSubmit={props.onSubmit} className="flex flex-col p-8 mx-auto my-4 bg-gray-200 rounded-lg" style={{width: '80%'}} >
-      <h2 className="mb-4 text-xl font-semibold">Add a new Cookie Stand</h2>
-      <div className="mb-4">
-        <input name="location" className="w-full h-10 pl-1" placeholder={`${props.lastLocation ? ` ${props.lastLocation}` : ''}`} />
-      </div>
-      <div className="flex justify-between space-x-2">
-        <div className="flex flex-col">
-          <label htmlFor="minCustomers">Minimum Customers per Hour</label>
-          <input type="number" name="minCustomers" className="flex-grow h-10 pl-1" placeholder="Minimum Customers per Hour" />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="maxCustomers">Maximum Customers per Hour</label>
-          <input type="number" name="maxCustomers" className="flex-grow h-10 pl-1" placeholder="Maximum Customers per Hour" />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="avgCookies">Average Cookies per Sale</label>
-          <input type="number" name="avgCookies" className="flex-grow h-10 pl-1" placeholder="Average Cookies per Sale" />
-        </div>
-        <button className="self-end h-10 px-2 bg-gray-500 text-gray-50">Create</button>
-      </div>
-    </form>
-  );
-}
-
-function CookieStandTable({ cookieStands }) {
-  return (
-    <table className="w-4/5 pt-4 pl-10 pr-10 mx-auto mt-4">
-      <thead>
-        <tr>
-          <th>Location</th>
-          <th>Min Customers/Hour</th>
-          <th>Max Customers/Hour</th>
-          <th>Avg Cookies/Sale</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cookieStands.map((stand, index) => (
-          <tr key={index}>
-            <td>{stand.location}</td>
-            <td>{stand.minCustomers}</td>
-            <td>{stand.maxCustomers}</td>
-            <td>{stand.avgCookies}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="fixed bottom-0 left-0 w-full p-1 text-xs text-center bg-green-900 text-gray-50">
-      <p>Cookie Stand &copy;{ new Date().getFullYear() }</p>
-    </footer>
-  );
-}
 
 function Placeholder({ lastLocation }) {
   return (
