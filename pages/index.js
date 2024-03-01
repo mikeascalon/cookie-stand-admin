004941c642b68f1eacc1768177ce0d6d4797c796
mikeascalon <mikeascalon@yahoo.com>
2024-02-29 19:41:12 -0800

    auth still not working

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React, {createContext, useContext, useState, useEffect  } from 'react';
 import Header from '@/components/header.js';
 import Footer from '@/components/footer.js';
 import QuestionForm from '@/components/questionForm.js';
 import CookieStandTable from '@/components/cookieStandTable.js';
 import { hours } from '../data';
+import { useAuth } from '../contexts/auth';
 
 
 function Main(props) {
@@ -15,6 +16,7 @@ export default function Home() {
 
   const [cookieStands, setCookieStands] = useState([]);
   const [lastLocation, setLastLocation] = useState('location');
+  const { user, login } = useAuth();
 
 
   const handleSubmit = (event) => {
@@ -32,6 +34,10 @@ export default function Home() {
     setLastLocation(newStand.location); 
     event.target.reset();
   };
+
+  const deleteStand = (locationToDelete) => {
+    setCookieStands(cookieStands.filter(stand => stand.location !== locationToDelete));
+  };
   
 
   return (
@@ -41,10 +47,19 @@ export default function Home() {
         <link rel="icon" href="/cookie.svg" />
 
       </Head>
-      <Header />
+      <Header>
+        {user ?
+                  <>
+                      <h1>Logged in: {user.email}</h1>
+                      <CookieStandAdmin />
+                  </>
+                  :
+                  <LoginForm onLogin={login} />
+              }
+      </Header>
       <Main>
         <QuestionForm onSubmit={handleSubmit} lastLocation={lastLocation} />
-        <CookieStandTable cookieStands={cookieStands} hours={hours} />
+        <CookieStandTable cookieStands={cookieStands} hours={hours} onDelete={deleteStand}/>
         
       </Main>
       <Footer />
@@ -59,5 +74,26 @@ function Placeholder({ lastLocation }) {
     <div className="fixed left-0 w-full p-1 text-xs text-center bg-gray-200 bottom-16">
       <p>Last created location: {lastLocation}</p>
     </div>
+  );
+}
+
+function LoginForm({ onLogin }) {
+
+  async function handleSubmit(event) {
+      event.preventDefault();
+      onLogin(event.target.username.value, event.target.password.value);
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+          <fieldset autoComplete='off'>
+              <legend>Log In</legend>
+              <label htmlFor="username">Username</label>
+              <input name="username" />
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+              <button>Log In</button>
+          </fieldset>
+      </form>
   );
 }
